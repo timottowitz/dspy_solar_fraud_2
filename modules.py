b13e7cbafa214e91cbd5f174f875492f76eadddd
timottowitz <tim.ottowitz@protonmail.com>
2025-06-20 17:37:24 -0600

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,27 @@
+# modules.py
+import dspy
+from . import signatures
+
+class SectionGenerator(dspy.Module):
+    def __init__(self):
+        super().__init__()
+        self.generate = dspy.ChainOfThought(signatures.GenerateSection)
+
+    def forward(self, client_facts, context, section_title):
+        return self.generate(
+            client_facts=client_facts,
+            context=context,
+            section_title=section_title
+        )
+
+class LegalArgumentGenerator(dspy.Module):
+    def __init__(self):
+        super().__init__()
+        self.generate = dspy.ChainOfThought(signatures.GenerateLegalCauseOfAction)
+
+    def forward(self, client_facts, cause_of_action, example_argument):
+        return self.generate(
+            client_facts=client_facts,
+            cause_of_action=cause_of_action,
+            example_argument=example_argument
+        )
